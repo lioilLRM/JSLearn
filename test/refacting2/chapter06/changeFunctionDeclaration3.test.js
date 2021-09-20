afe55110db56801ab029de7994843e4546b07b2a
lrmlioil <840687434@qq.com>
2021-09-21 06:44:07 +0800

    修改：1.封装变量

---
+++ test/refacting2/chapter06/changeFunctionDeclaration3.test.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai'
+import { isNewEngland } from '../../../refacting2/chapter06/01-extract-function/changeFunctionDeclaration3.js'
+
+describe('isNewEngland', () => {
+  it('', () => {
+
+  })
+})
