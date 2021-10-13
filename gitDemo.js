2616cb0a5d4b4eafef42ed1e34586aecdc6e37b8
lrmlioil <840687434@qq.com>
2021-10-13 23:19:25 +0800

    暂存

--- gitDemo.js
+++ gitDemo.js
@@ -1,4 +1,14 @@
 const temp = 'git demo'
 
+<<<<<<< Updated upstream
 const master = 'master bug fix'
 const masterFix = 'matster bug fix2'
+=======
+const gitTest = 'temp'
+
+const git = 'git reset HEAD^'
+const master = 'master bug fix'
+
+const addConent = 'new Content'
+const stashchagne = 'stash test'
+>>>>>>> Stashed changes
