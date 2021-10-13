9aea4f60fa10ec4e16d54c4cb0c48b8463394079
lrmlioil <840687434@qq.com>
2021-10-13 23:29:13 +0800

    修改：1.bug fix3

--- gitDemo.js
+++ gitDemo.js
@@ -1,14 +1,6 @@
 const temp = 'git demo'
 
-<<<<<<< Updated upstream
 const master = 'master bug fix'
 const masterFix = 'matster bug fix2'
-=======
-const gitTest = 'temp'
 
-const git = 'git reset HEAD^'
-const master = 'master bug fix'
-
-const addConent = 'new Content'
-const stashchagne = 'stash test'
->>>>>>> Stashed changes
+const masterFix3 = 'master bug fix3'
